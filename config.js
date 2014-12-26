dd1d47347a4564c1dc44cbc31f7f8e0d84b4d7db
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2014-12-26 15:08:30 +0300

    Changed url in config file

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://anastasiazotova.azurewebsites.net/',
+        url: 'http://anastasiazotova.com/',
         mail: {},
         database: {
             client: 'sqlite3',
